2b5e5d5f235d87878b95a683e5f7bf0b05fcb491
Federico <federico__russo@live.it>
2022-03-01 00:44:58 +0100

    [CONNECTION][WORKER] Fixed start_link (previously not starting correctly); addded "username_available" API; added logging to user_login API

--- fr_miniclip_challenge/src/connection_manager.erl
+++ fr_miniclip_challenge/src/connection_manager.erl
@@ -17,7 +17,7 @@
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
   code_change/3]).
--export([user_login/2, send_message_to_user/3]).
+-export([user_login/2, send_message_to_user/3, username_available/1, logged_users/0]).
 
 -define(SERVER, ?MODULE).
 
@@ -31,7 +31,7 @@
 -spec(start_link() ->
   {ok, Pid :: pid()} | ignore | {error, Reason :: term()}).
 start_link() ->
-  gen_server:start_link(?MODULE, ?MODULE, []).
+  gen_server:start_link({local, ?SERVER},?MODULE, [], []).
 
 %%%===================================================================
 %%% gen_server callbacks
@@ -43,13 +43,22 @@ start_link() ->
   {ok, State :: #state{}} | {ok, State :: #state{}, timeout() | hibernate} |
   {stop, Reason :: term()} | ignore).
 init([]) ->
+  io:format("Started module ~s (under supervisor) for user connection management~n", [?MODULE]),
   {ok, #state{}}.
 
 user_login(Pid, Username) ->
+  io:format("Logging in user ~s~n", [Username]),
   gen_server:call(self(), {add, Pid, Username}).
 
 send_message_to_user(Payload, Recipient, Sender) ->
   gen_server:call(self(), {message, Recipient, Payload, Sender}).
+
+username_available(Username) ->
+  io:format("Checking availability of username ~s~n", [Username]),
+  gen_server:call(self(), {available, Username}).
+
+logged_users() ->
+  gen_server:call(self(), {list}).
 
 %% @private
 %% @doc Handling call messages
@@ -77,6 +86,16 @@ handle_call({message, Username, Payload, Sender}, _From, State) ->
       client:receive_message(Pid, Payload, Sender),
       {reply, "200 OK", State}
   end;
+handle_call({available, Username}, _From, State = #state{names = List}) ->
+  UserExists = lists:member(Username, List),
+  if UserExists =:= false ->
+    {reply, available, State};
+    true ->
+      {reply, not_available, State}
+  end;
+handle_call({list}, _From, State = #state{names = List}) ->
+  io:format("Logged users are ~p~n", [List]),
+  {reply, "204 - No content", State};
 handle_call(_Request, _From, State = #state{}) ->
   {reply, ok, State}.
 
