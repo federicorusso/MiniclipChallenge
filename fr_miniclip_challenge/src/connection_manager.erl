2a72f4b0ef7877e06da720fb8ae3cee5aeb9c99b
Federico <federico__russo@live.it>
2022-02-28 01:10:37 +0100

    [APP][CONNECTION] Added connection manager & supervisor - they shall provide client modules information about each other's whereabouts (i.e. return the PID by Username so to forward messages). THIS WAS NOT INCLUDED IN THE BUILD NOR IN PRE-COMMITTED MODULES

---
+++ fr_miniclip_challenge/src/connection_manager.erl
@@ -0,0 +1,129 @@
+%%%-------------------------------------------------------------------
+%%% @author feder
+%%% @copyright (C) 2022, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 28. feb 2022 00:41
+%%%-------------------------------------------------------------------
+-module(connection_manager).
+-author("feder").
+
+-behaviour(gen_server).
+
+%% API
+-export([start_link/0]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
+  code_change/3]).
+-export([user_login/2, send_message_to_user/3]).
+
+-define(SERVER, ?MODULE).
+
+-record(state, {names = [], records = []}).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%% @doc Spawns the server and registers the local name (unique)
+-spec(start_link() ->
+  {ok, Pid :: pid()} | ignore | {error, Reason :: term()}).
+start_link() ->
+  gen_server:start_link(?MODULE, ?MODULE, []).
+
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
+
+%% @private
+%% @doc Initializes the server
+-spec(init(Args :: term()) ->
+  {ok, State :: #state{}} | {ok, State :: #state{}, timeout() | hibernate} |
+  {stop, Reason :: term()} | ignore).
+init([]) ->
+  {ok, #state{}}.
+
+user_login(Pid, Username) ->
+  gen_server:call(self(), {add, Pid, Username}).
+
+send_message_to_user(Payload, Recipient, Sender) ->
+  gen_server:call(self(), {message, Recipient, Payload, Sender}).
+
+%% @private
+%% @doc Handling call messages
+-spec(handle_call(Request :: term(), From :: {pid(), Tag :: term()},
+    State :: #state{}) ->
+  {reply, Reply :: term(), NewState :: #state{}} |
+  {reply, Reply :: term(), NewState :: #state{}, timeout() | hibernate} |
+  {noreply, NewState :: #state{}} |
+  {noreply, NewState :: #state{}, timeout() | hibernate} |
+  {stop, Reason :: term(), Reply :: term(), NewState :: #state{}} |
+  {stop, Reason :: term(), NewState :: #state{}}).
+
+handle_call({add, Pid, Username}, _From, State = #state{names = List, records = Users}) ->
+  UserExists = lists:member(Username, List),
+  if UserExists =:= false ->
+    {reply, "200 - Added", #state{names = [Username | List], records = [{Username, Pid} | Users]}};
+    true ->
+      {reply, "404 - Invalid username", State}
+  end;
+handle_call({message, Username, Payload, Sender}, _From, State) ->
+  Pid = get_pid_by_username(Username, State),
+  if Pid == [] ->
+    {reply, "404 - Invalid username", State};
+    true ->
+      client:receive_message(Pid, Payload, Sender),
+      {reply, "200 OK", State}
+  end;
+handle_call(_Request, _From, State = #state{}) ->
+  {reply, ok, State}.
+
+%% @private
+%% @doc Handling cast messages
+-spec(handle_cast(Request :: term(), State :: #state{}) ->
+  {noreply, NewState :: #state{}} |
+  {noreply, NewState :: #state{}, timeout() | hibernate} |
+  {stop, Reason :: term(), NewState :: #state{}}).
+handle_cast(_Request, State = #state{}) ->
+  {noreply, State}.
+
+%% @private
+%% @doc Handling all non call/cast messages
+-spec(handle_info(Info :: timeout() | term(), State :: #state{}) ->
+  {noreply, NewState :: #state{}} |
+  {noreply, NewState :: #state{}, timeout() | hibernate} |
+  {stop, Reason :: term(), NewState :: #state{}}).
+handle_info(_Info, State = #state{}) ->
+  {noreply, State}.
+
+%% @private
+%% @doc This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any
+%% necessary cleaning up. When it returns, the gen_server terminates
+%% with Reason. The return value is ignored.
+-spec(terminate(Reason :: (normal | shutdown | {shutdown, term()} | term()),
+    State :: #state{}) -> term()).
+terminate(_Reason, _State = #state{}) ->
+  ok.
+
+%% @private
+%% @doc Convert process state when code is changed
+-spec(code_change(OldVsn :: term() | {down, term()}, State :: #state{},
+    Extra :: term()) ->
+  {ok, NewState :: #state{}} | {error, Reason :: term()}).
+code_change(_OldVsn, State = #state{}, _Extra) ->
+  {ok, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+
+get_pid_by_username(Username, _State = #state{names = List, records = Users}) ->
+  UserExists = lists:member(Username, List),
+  if UserExists =:= false ->
+    [];
+    true ->
+      [ Pid || {Name, Pid} <- Users, Name == Username ]
+  end.
---
+++ fr_miniclip_challenge/src/connection_manager_supervisor.erl
@@ -0,0 +1,28 @@
+%%%-------------------------------------------------------------------
+%%% @author feder
+%%% @copyright (C) 2022, <COMPANY>
+%%% @doc
+%%% @end
+%%%-------------------------------------------------------------------
+-module(connection_manager_supervisor).
+
+-behaviour(supervisor).
+
+-export([start_link/0, init/1]).
+
+start_link() ->
+  supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+init([]) ->
+  AChild = #{id => 'connection_manager',
+    start => {'connection_manager', start_link, []},
+    restart => permanent,
+    shutdown => 2000,
+    type => worker,
+    modules => ['connection_manager']},
+
+  {ok, {#{strategy => one_for_one,
+    intensity => 5,
+    period => 30},
+    [AChild]}
+  }.
